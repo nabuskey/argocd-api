cdfb98d79358d0a2614290c4abc1b8b213aa1602
Manabu McCloskey <manabu.mccloskey@gmail.com>
2024-02-07 11:49:04 -0700

    Update register.go

--- api/argo/application/v1alpha1/register.go
+++ api/argo/application/v1alpha1/register.go
@@ -1,7 +1,7 @@
 package v1alpha1
 
 import (
-	"github.com/cnoe-io/argocd-api/api/argo/application"
+	"github.com/nabuskey/argocd-api/api/argo/application"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
